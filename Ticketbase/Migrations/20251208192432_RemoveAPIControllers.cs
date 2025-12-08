e1b84f53d801c41d9b44c6400844b945eee8ec17
lucrb <luc.r.brousseau@gmail.com>
2025-12-08 15:25:27 -0400

    Remove api controllers

---
+++ Ticketbase/Migrations/20251208192432_RemoveAPIControllers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticketbase.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAPIControllers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
